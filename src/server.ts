6d7977886fea1bc025bc07292baf669a8016e4a6
Paulo-Viana <paulodev1612@gmail.com>
2022-02-24 17:01:58 -0300

    desacoplando cada vez mais as funcionalidades da aplicação usando o padrão de arquitetura SOLID

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { categoriesRoutes } from './routes/categories.routes'
+import { specificationsRoutes } from './routes/specifications.routes'
 
 const app = express()
 const port = 3333
@@ -8,6 +9,7 @@ app.use(express.json())
 
 //ROUTES
 app.use('/categories', categoriesRoutes)
+app.use('/specifications', specificationsRoutes)
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`)
